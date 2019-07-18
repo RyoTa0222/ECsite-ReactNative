891e48ed24097ce93af7fc8243bb1014160b30fc
matsumotoryouta <r.matsumoto.creative.innovation@gmail.com>
2019-07-18 16:25:19 +0900

    firebaseとの連携

--- App.js
+++ App.js
@@ -1,5 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Icon, Body, Text, Form, Item, Input, Label, Spinner } from 'native-base';
+
+import firebase from 'react-native-firebase';
+
+// rules_version = '2';
+// service cloud.firestore {
+//   match / databases / { database } / documents {
+//     match / { document=**} {
+//       allow read, write;
+//     }
+//   }
+// }
+var firebaseConfig = {
+  apiKey: "AIzaSyD5SK5js8alQ_JE2Dqz3NiFB7Hs8nvKGJk",
+  authDomain: "expo-ec.firebaseapp.com",
+  databaseURL: "https://expo-ec.firebaseio.com",
+  projectId: "expo-ec",
+  storageBucket: "expo-ec.appspot.com",
+  messagingSenderId: "979611158988",
+  appId: "1:979611158988:web:707334eb030667a7"
+};
+firebase.initializeApp(firebaseConfig);
 
 export default function App() {
   return (
